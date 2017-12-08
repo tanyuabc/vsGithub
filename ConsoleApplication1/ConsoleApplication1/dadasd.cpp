83585c9236b91535b93859c0afc525e9158011be
github <github>
2017-12-08 16:43:06 +0800

    adsfaf

---
+++ ConsoleApplication1/ConsoleApplication1/dadasd.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "dadasd.h"
+
+
+dadasd::dadasd()
+{
+}
+
+
+dadasd::~dadasd()
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/dadasd.h
@@ -0,0 +1,8 @@
+#pragma once
+class dadasd
+{
+public:
+	dadasd();
+	~dadasd();
+};
+
